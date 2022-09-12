8b12a351a7af3b6a8b9bb47bb782435c666047c5
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-09-12 15:12:29 -0400

    Updated scripts

---
+++ scripts/enrichment.py
@@ -0,0 +1,82 @@
+#### terminal commands ####
+
+'pip3 install pandas'
+
+'pip3 install numpy'
+
+
+
+#### import packages ####
+
+from operator import concat
+import pandas as pd
+
+import numpy as np
+
+
+
+#### import csv as dataframes ####
+
+sparcs_df = pd.read_csv('data/raw/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv')
+
+sparcs_df.sample
+
+adi_df = pd.read_csv('data/raw/NY_2015_ADI_9 Digit Zip Code_v3.1.csv')
+
+adi_df
+
+#### pre merge clean-up ####
+
+sparcs_df.columns
+
+adi_df.columns
+
+## in the interest of file size and functionality, let's drop a few columns from each df
+
+sparcs_df = sparcs_df.drop(columns=['Patient Disposition', 'Discharge Year', 'APR Medical Surgical Description', 'Payment Typology 2', 'Payment Typology 3', 'Birth Weight', 'Abortion Edit Indicator', 'Emergency Department Indicator', 'Total Charges'])
+
+sparcs_df.columns
+
+adi_df = adi_df.drop(columns=['Unnamed: 0', 'X'])
+
+adi_df.columns
+
+## let's do some best practice cleaning with column names and element types
+
+sparcs_df.columns = sparcs_df.columns.str.lower()
+
+sparcs_df.columns
+
+adi_df.columns = adi_df.columns.str.lower()
+
+adi_df.columns
+
+sparcs_df.columns = sparcs_df.columns.str.replace(' ', '_')
+
+sparcs_df.columns
+
+sparcs_df = sparcs_df.rename(columns={'payment_typology_1':'payment_typology'})
+## now that we only have primary payment type left, we can drop the '1'
+
+adi_df.columns = adi_df.columns.str.replace(' ', '_')
+
+adi_df.columns
+
+sparcs_df.dropna(inplace=True)
+
+adi_df.dropna(inplace=True)
+
+## because this is a practice repo and these file sizes are huge, we can afford to drop rows with NaN
+
+sparcs_df.drop(sparcs_df.loc[552311:2311006].index, inplace=True)
+
+sparcs_df.shape
+
+adi_df.shape
+
+enrich_df = pd.concat([sparcs_df, adi_df])
+
+enrich_df
+
+## we are running into an error in which the concat or merge operation goes through, but columns from adi are labeled NaN. This may occur because of mismatched dtypes.
+## The attempted solution to this was to match row numbers through drop. This did not remedy the error.
